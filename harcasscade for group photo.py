d725344f06eacb6c572ce9e5de52bb58f672ec2d
Aditya Narayan Pradhan <44144530+Adityanrnprdn@users.noreply.github.com>
2020-01-04 13:53:02 +0530

    Add files via upload

---
+++ harcasscade for group photo.py
@@ -0,0 +1,18 @@
+import cv2
+ #load the cascade
+face_cascade = cv2.CascadeClassifier(r"C:\Users\cttc\Desktop\ADITYA AI&ML COURSE\Datasets\data\haarcascades\haarcascade_frontalface_alt.xml")
+ 
+ #Read the input image
+img = cv2.imread(r"C:\Users\cttc\Desktop\ADITYA AI&ML COURSE\testimage7.jpg")
+ 
+ #convert into grayscale
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ #Detect faces
+faces = face_cascade.detectMultiScale(gray)
+ #Draw rectangle around the faces
+for (x , y, w,h) in faces:
+    cv2.rectangle(img, (x,y),(x+w ,y+h),(255,0,0),2)
+#Display the output
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
